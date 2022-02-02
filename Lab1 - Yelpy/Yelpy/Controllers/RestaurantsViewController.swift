1948640c13b77f63f5b8410c0cc3d21ff038dd45
Christian Franklin <christian.m.franklin@gmail.com>
2022-02-01 23:14:30 -0500

    Created Yelpy Codepath iOS project. Lab 1.

---
+++ Lab1 - Yelpy/Yelpy/Controllers/RestaurantsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Yelpy
+//
+//  Created by Memo on 5/21/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class RestaurantsViewController: UIViewController {
+    
+    // ––––– TODO: Add storyboard Items (i.e. tableView + Cell + configurations for Cell + cell outlets)
+    // ––––– TODO: Next, place TableView outlet here
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // –––––– TODO: Initialize restaurantsArray
+    var restaurantsArray: [[String:Any?]] = []
+    
+    
+    // ––––– TODO: Add tableView datasource + delegate
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    
+    // ––––– TODO: Get data from API helper and retrieve restaurants
+    func getAPIData(){
+        API.getRestaurants() {
+            (restaurants) in guard let restaurants = restaurants else {
+                return
+            }
+            print(restaurants)
+            self.restaurants = restaurants
+        }
+    }
+
+}
+
+// ––––– TODO: Create tableView Extension and TableView Functionality
+
+
