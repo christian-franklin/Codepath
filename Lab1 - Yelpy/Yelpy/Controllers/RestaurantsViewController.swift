a4c487429c9c078a86e2fd1e2875a75557c09613
Christian Franklin <christian.m.franklin@gmail.com>
2022-02-02 00:42:05 -0500

    Completed Lab 1 - Yelpy

--- Lab1 - Yelpy/Yelpy/Controllers/RestaurantsViewController.swift
+++ Lab1 - Yelpy/Yelpy/Controllers/RestaurantsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import AlamofireImage
 
 class RestaurantsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
     
     // ––––– TODO: Add storyboard Items (i.e. tableView + Cell + configurations for Cell + cell outlets)
     // ––––– TODO: Next, place TableView outlet here
@@ -26,7 +27,7 @@ class RestaurantsViewController: UIViewController, UITableViewDelegate, UITableV
         getAPIData()
         tableView.delegate = self
         tableView.dataSource = self
-
+        tableView.rowHeight = 150
     }
     
     
@@ -36,32 +37,36 @@ class RestaurantsViewController: UIViewController, UITableViewDelegate, UITableV
             (restaurants) in guard let restaurants = restaurants else {
                 return
             }
+            //print(restaurants)
             self.restaurantsArray = restaurants
             self.tableView.reloadData() // reload data!
         }
     }
 
-}
+
 
 // ––––– TODO: Create tableView Extension and TableView Functionality
-
 func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return restaurantArray.count
+    return restaurantsArray.count
 }
 
 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    // Create Restaurant Cell
+    //Create Restaurant Cell
     let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell") as! RestaurantCell
     
     let restaurant = restaurantsArray[indexPath.row]
     
-    //Set Label to restaurant name for each cell
+    // Set Label to restaurant name for each cell
     cell.label.text = restaurant["name"] as? String ?? ""
     
-    //Set Image of restaurant
+    // Set Image of restaurant
     if let imageUrlString = restaurant["image_url"] as? String {
         let imageUrl = URL(string: imageUrlString)
-        cell.restaurantImage.af.setImage(withURL: imageUrl!)
+        cell.RestaurantImage.af.setImage(withURL: imageUrl!)
     }
+    
     return cell
+}
+
+    
 }
--- Lab1 - Yelpy/Yelpy/Network/API.swift
+++ Lab1 - Yelpy/Yelpy/Network/API.swift
@@ -41,12 +41,12 @@ struct API {
 
                 // ––––– TODO: Get data from API and return it using completion
                 print(data)
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options:[] as! [String: Any])
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 
                 let restaurants = dataDictionary["businesses"] as! [[String: Any]]
                 
                 
-                return completion([[:]])
+                return completion(restaurants)
                 
                 }
             }
